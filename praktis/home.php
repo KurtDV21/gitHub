64025c0d1b3defcb28a67beb8054d9be58a32faa
suuupperrb <delosreyesaaron.bsit@gmail.com>
2024-10-03 00:53:02 +0800

    Add files via upload

---
+++ praktis/home.php
@@ -0,0 +1,125 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Aoboshi+One&display=swap">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<!-- header nav-bar -->
+<header>
+  <nav class="navbar">
+    <img src="logo1.png" alt="logo" class="logo">
+
+    <div class="nav-container"> <!-- New div to contain nav links -->
+      <ul class="nav-link">
+        <li><a href="#home">HOME</a></li>
+        <li><a href="">OUR CATS</a></li>
+        <li><a href="">ABOUT</a></li>
+        <li><a href="">FAQs</a></li>
+
+      
+      </ul>
+      <button class="login-btn"> <a href="login.php">Login</a></button>
+    </div>
+  </nav>
+</header>
+
+
+ 
+  </div>
+</div>
+
+<!-- body -->
+
+
+      <!-- HOME SECTION -->
+        <section id="home">
+          <div class="main">
+            <h1 class="text1">Find Your Purr-fect<br> Companion: Adopt<br> a Cat Today!</h1>
+          </div>
+
+
+          <div class="adopt-btn-container">
+            <button class="adopt-btn button">ADOPT NOW!</button>
+            </div>
+        </section>
+
+
+          <!-- OUR CATS SECTION -->
+          <section id="ourcat">
+            <div class="dropdown">
+                <button onclick="myFunction()" class="dropbtn"><b>Color</b></button>
+                <div id="myDropdown" class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                </div>
+            </div>
+
+              <div class="dropdown">
+                 <button onclick="myFunctionGender()" class="dropbtn"><b>Gender</b></button>
+                  <div id="myDropdownGender" class="dropdown-content">
+                        <a href="#">Male</a>
+                        <a href="#">Female</a>
+                        <a href="#">Other</a>
+                  </div>
+              </div>
+              
+              
+              <button class="post-btn"><b>Add Post</b></button>
+
+            
+          <div class="picture-box-container">
+            <div class="pic-box">
+             <div class="picture-box">
+             <img src="your-image.jpg" alt="Your Image Description">
+             </div>
+               
+
+              </div>
+              <div class="info-box">
+              <h2>CATS NAME</h2>
+              <p>ASDASDASDASD</p>
+                </div>
+              
+        </div>
+
+
+        <div class="picture-box-container">
+            <div class="pic-box">
+             <div class="picture-box">
+             <img src="your-image.jpg" alt="Your Image Description">
+             </div>
+               
+
+              </div>
+              <div class="info-box">
+              <h2>CATS NAME</h2>
+              <p>ASDASDASDASD</p>
+                </div>
+              
+        </div>
+        
+
+        </section>
+    
+   
+
+    
+
+
+
+
+
+
+
+
+<script src="package.js"></script>
+</body>
+</html>
